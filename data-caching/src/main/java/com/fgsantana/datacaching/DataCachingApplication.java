850a02567994b3c99081c2b9ec0f05099b9c06e3
felipe gomes <fgss@cin.ufpe.br>
2022-05-23 02:54:07 -0300

    Spring Boot project

---
+++ data-caching/src/main/java/com/fgsantana/datacaching/DataCachingApplication.java
@@ -0,0 +1,14 @@
+package com.fgsantana.datacaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCachingApplication.class, args);
+	}
+
+
+}
---
+++ data-caching/src/test/java/com/fgsantana/datacaching/DataCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fgsantana.datacaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
